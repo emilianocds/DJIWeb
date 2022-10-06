0c309575b9b16048ce94c2d0b9f8903089919114
M-borba <mborbacds@gmail.com>
2022-10-06 12:24:34 -0300

    adding shapes to state, todo: remove and clear

--- src/components/DrawableGMap.vue
+++ src/components/DrawableGMap.vue
@@ -1,5 +1,7 @@
 
 <template>
+  <button class="clear" v-on:click="clearShapes">Clear</button>
+  <button class="clear" v-on:click="deleteShape(0)">Remove</button>
     <div id="map"></div>
 </template>
 
@@ -7,20 +9,53 @@
 // done with https://developers.google.com/maps/documentation/javascript/drawinglayer library
 // +docs https://developers.google.com/maps/documentation/javascript/reference/drawing
 import { Loader } from 'google-maps'
+import { defineComponent } from 'vue'
+
 const apiKey = import.meta.env.GOOGLE_MAPS_API_KEY || 'AIzaSyCWjSQkpYWRMa93lsB6UbQ8jeEWtH7J43s'
 const options = { libraries: ['drawing'] }
 const loader = new Loader(apiKey, options)
 
-export default {
+export default defineComponent({
+  el: '#gmaps',
   name: 'googleMaps',
+  data: function () {
+    return {
+      overlayShapes: [],
+      drawingManager: {},
+      initialMap: {},
+    }
+  },
+  methods: {
+    addShape: function (shape) {
+      this.overlayShapes.push(shape)
+    },
+
+    deleteShape: function (index) {
+      if (index < this.overlayShapes.length) {
+        console.log('this.overlayShapes', this.overlayShapes[index].setMap)
+        this.overlayShapes[index].setMap(null)
+        this.overlayShapes.splice(index, 1)
+        console.log('this.overlayShapes', this.overlayShapes)
+      }
+    },
+
+    clearShapes: function () {
+      console.log('clearMap')
+      if (confirm('Want to clear all overlayShapes?')) {
+        this.overlayShapes = []
+        // TODO: Clear all somehow, worst case: iterate over all shapes and remove one by one
+      }
+    },
+  },
   mounted: async function () {
     const google = await loader.load()
-    const map = new google.maps.Map(document.getElementById('map'), {
+    this.initialMap = new google.maps.Map(document.getElementById('map'), {
       center: { lat: -34.88338238343086, lng: -56.14534138394627 }, // INITIAL POSITION (MONTEVIDEO)
       zoom: 10
     })
+    this.overlayShapes = []
     // add drawing menu
-    const drawingManager = new google.maps.drawing.DrawingManager({
+    this.drawingManager = new google.maps.drawing.DrawingManager({
       drawingMode: google.maps.drawing.OverlayType.MARKER,
       drawingControl: true,
       drawingControlOptions: {
@@ -37,35 +72,43 @@ export default {
         strokeWeight: 1,
         clickable: false,
         editable: true,
+        draggable: true,
         zIndex: 1
       },
       polygonOptions: {
         strokeWeight: 1,
         clickable: false,
         editable: true,
+        draggable: true,
         zIndex: 1
       },
       polylineOptions: {
         strokeWeight: 2,
         clickable: false,
         editable: true,
+        draggable: true,
         zIndex: 1
       },
       rectangleOptions: {
         strokeWeight: 2,
         clickable: false,
         editable: true,
+        draggable: true,
         zIndex: 1
       }
     })
+
     // particular event handler a overlay object on complete
-    google.maps.event.addListener(drawingManager, 'circlecomplete', function (circle) {
+    google.maps.event.addListener(this.drawingManager, 'circlecomplete', (circle) => {
       const radius = circle.getRadius()
-      console.log('circle overlay listener', circle)
+      // circle.setMap(null) this does remove the circle
     })
     // callback when any overlay is completed
-    google.maps.event.addListener(drawingManager, 'overlaycomplete', function (event) {
+    google.maps.event.addListener(this.drawingManager, 'overlaycomplete', (event) => {
       console.log('overlaycomplete event type:', event.type)
+      console.log('overlay object:', event.overlay)
+
+      this.overlayShapes.push(event.overlay)
 
       if (event.type === 'marker') {
         console.log('marker', event.overlay)
@@ -83,7 +126,8 @@ export default {
         console.log('rectangle', event.overlay)
       }
     })
-    drawingManager.setMap(map)
+    this.drawingManager.setMap(this.initialMap)
+
     //  CONTROLS ARE EVEN MOVABLE :
     // drawingManager.setOptions({
     //   drawingControlOptions: {
@@ -91,11 +135,15 @@ export default {
     //   }
     // })
   }
-}
+})
 </script>
 
 <style scoped>
 #map {
   height:100%;
+}
+.clear{
+  background-color:crimson;
+  color:white;
 }
 </style>
